db5b7d48db1058ef9de801842cb6431ec7a46fa8
aliaga <aliaga.a@code.edu.az>
2016-08-09 15:07:16 +0400

    before highlght update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
         <script src="/assets/js/main.js"></script>
 
         <script src="/js/highlight.js/lib/highlight.js"></script>
+        <script>hljs.initHighlightingOnLoad();</script>
         <script src="/vendors/ckeditor/ckeditor/ckeditor.js"></script>
 
 
@@ -132,7 +133,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
         {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-        <script>hljs.initHighlightingOnLoad();</script>
+        
         <script>
              var config = {
                         extraPlugins: 'codesnippet',
